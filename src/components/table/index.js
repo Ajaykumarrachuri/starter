4196e1927d4c0a7cb30c8d3e0986349c17d661ee
AJAY RACHURI <ajaykumar.rachuri@gmail.com>
2021-05-22 21:37:33 +0530

    added react-table library (#10)

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,37 +1,51 @@
+/* eslint-disable react/jsx-key */
 import { array } from "prop-types";
 import React from "react";
+import { useTable } from "react-table";
 
 // adjustable params
 // columns, rows, sortable by coulmn, pagination, background color
-const Table = ({ data }) => {
+const Table = ({ columns, data }) => {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
   return (
-    <div className="bg-blue-300 rounded-lg m-5">
-      <table className="table-fixed m-10">
-        <thead>
-          <tr>
-            <th className="w-1/2 ...">Title</th>
-            <th className="w-1/4 ...">Author</th>
-            <th className="w-1/4 ...">Views</th>
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
           </tr>
-        </thead>
-        <tbody>
-          {data.map(({ name, currency, updated }, index) => {
-            return (
-              <tr key={index}>
-                <td>{name}</td>
-                <td>{currency}</td>
-                <td>{updated}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
 Table.propTypes = {
   data: array,
+  columns: array,
+};
+
+Table.defaultProps = {
+  data: [],
+  columns: [],
 };
 
 export default Table;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import Table from "../../components/table";
 import { request } from "../../library/request";
 
 const home = () => {
-  const [state, setstate] = useState({ tickers: [] });
+  const [data, setData] = useState([]);
+  const [isloading, setloading] = useState(false);
+  const columns = useMemo(
+    () => [
+      {
+        Header: "data-table",
+        columns: [
+          {
+            Header: "Name",
+            accessor: "name",
+          },
+          {
+            Header: "Currency",
+            accessor: "currency",
+          },
+          {
+            Header: "Updated",
+            accessor: "updated",
+          },
+        ],
+      },
+    ],
+    []
+  );
   useEffect(async () => {
+    setloading(true);
     const response = await request({
       method: "GET",
       url: "https://api.polygon.io/v2/reference/tickers",
@@ -16,11 +40,12 @@ const home = () => {
       },
     });
     console.log(response);
-    setstate(response.data);
+    setData(response.data.tickers);
+    setloading(false);
   }, []);
   return (
     <div className="text-2xl font-serif font-bold">
-      <Table data={state.tickers} />
+      {!isloading && <Table columns={columns} data={data} />}
     </div>
   );
 };
